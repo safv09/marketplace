295008deac300856051aedc6ffe5fec1c5dd035d
safv09 <saf99016@gmail.com>
2026-05-22 11:55:14 -0700

    SPRINT 3 - Gestión de Productos + Dashboard

--- marketplace_main/settings.py
+++ marketplace_main/settings.py
@@ -113,11 +113,15 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_URL = 'login'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- store/forms.py
+++ store/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+from .models import User,Product
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,3 +9,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'is_seller', 'password1', 'password2')
+
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'stock', 'categories']
+        widgets = {
+            'categories': forms.CheckboxSelectMultiple()
+        }
